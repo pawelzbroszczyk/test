b534b724836b443fc680f233f2d8a96785b2889a
pawelzbroszczyk <65468010+pawelzbroszczyk@users.noreply.github.com>
2020-05-17 19:58:22 +0200

    ograniczenie ruchu tylko do ekranu

--- test/Assets/Scripts/Player.cs
+++ test/Assets/Scripts/Player.cs
@@ -22,6 +22,8 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0); //określenie ruchu gracza
-        transform.Translate(direction * playerSpeed * Time.deltaTime); //wprawienie w ruch gracza z prędkością 'playerSpeed'        
+        transform.Translate(direction * playerSpeed * Time.deltaTime); //wprawienie w ruch gracza z prędkością 'playerSpeed'  
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.7f, 9.7f), Mathf.Clamp(transform.position.y, -4.3f, 6f), transform.position.z); //nie pozwol wyjsc graczowi poza ekran        
     }
+
 }
