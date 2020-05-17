4a77ea0c442ec4752f9404a1aa15daeb97d1e64b
Grzegorz <gwiniszewski1975@gmail.com>
2020-05-17 21:30:07 +0200

    zmiany

--- test/Assets/Scripts/Player.cs
+++ test/Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 5f;
+    [SerializeField] 
+    public float playerSpeed = 5f;
     private float minXPositionPossible=-8f, maxXPositionPossible=8.5f, minYPositionPossible=-4.5f, maxYPositionPossible=5; 
 
     void Start()
@@ -22,9 +23,10 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(0, verticalInput, horizontalInput); //określenie ruchu gracza (ponieważ obiekt jest odwrócony Y Rotation = 90 - horizontalInput wpisany jest w osi Z)
+        Vector3 direction = new Vector3(verticalInput, -horizontalInput, 0);
         transform.Translate(direction * playerSpeed * Time.deltaTime); //wprawienie w ruch gracza z prędkością 'playerSpeed'  
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minXPositionPossible, maxXPositionPossible), Mathf.Clamp(transform.position.y, minYPositionPossible, maxYPositionPossible), transform.position.z); //nie pozwol wyjsc graczowi poza ekran (może lepiej to ograniczyć innym obiektem z Colliderem?)       
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minXPositionPossible, maxXPositionPossible), 
+            Mathf.Clamp(transform.position.y, minYPositionPossible, maxYPositionPossible), transform.position.z); //nie pozwol wyjsc graczowi poza ekran (może lepiej to ograniczyć innym obiektem z Colliderem?)       
     }
 
 }
