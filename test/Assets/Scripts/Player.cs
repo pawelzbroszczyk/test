775c0207d630c7da1ea7f7385560306986a64fc4
pawelzbroszczyk <65468010+pawelzbroszczyk@users.noreply.github.com>
2020-05-17 20:30:36 +0200

    Free starship model added

--- test/Assets/Scripts/Player.cs
+++ test/Assets/Scripts/Player.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 3.5f;
-   
+    [SerializeField] float playerSpeed = 5f;
+    private float minXPositionPossible=-8f, maxXPositionPossible=8.5f, minYPositionPossible=-4.5f, maxYPositionPossible=5; 
+
     void Start()
     {
         
@@ -21,9 +22,9 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0); //określenie ruchu gracza
+        Vector3 direction = new Vector3(0, verticalInput, horizontalInput); //określenie ruchu gracza (ponieważ obiekt jest odwrócony Y Rotation = 90 - horizontalInput wpisany jest w osi Z)
         transform.Translate(direction * playerSpeed * Time.deltaTime); //wprawienie w ruch gracza z prędkością 'playerSpeed'  
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.7f, 9.7f), Mathf.Clamp(transform.position.y, -4.3f, 6f), transform.position.z); //nie pozwol wyjsc graczowi poza ekran        
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minXPositionPossible, maxXPositionPossible), Mathf.Clamp(transform.position.y, minYPositionPossible, maxYPositionPossible), transform.position.z); //nie pozwol wyjsc graczowi poza ekran (może lepiej to ograniczyć innym obiektem z Colliderem?)       
     }
 
 }
