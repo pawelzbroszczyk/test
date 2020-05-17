2a03fda3b57f6baa8b5be1302223118335ebe62d
pawelzbroszczyk <65468010+pawelzbroszczyk@users.noreply.github.com>
2020-05-17 21:47:14 +0200

    drugi player2

--- test/Assets/Scripts/Player.cs
+++ test/Assets/Scripts/Player.cs
@@ -23,7 +23,7 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(verticalInput, -horizontalInput, 0);
+        Vector3 direction = new Vector3(0, verticalInput, horizontalInput);
         transform.Translate(direction * playerSpeed * Time.deltaTime); //wprawienie w ruch gracza z prędkością 'playerSpeed'  
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, minXPositionPossible, maxXPositionPossible), 
             Mathf.Clamp(transform.position.y, minYPositionPossible, maxYPositionPossible), transform.position.z); //nie pozwol wyjsc graczowi poza ekran (może lepiej to ograniczyć innym obiektem z Colliderem?)       
